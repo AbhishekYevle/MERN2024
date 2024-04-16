a8358f0aa1f12b1e343b2037beb25001f36c2868
Palak Naidu <naidupalak14@gmail.com>
2024-04-16 15:22:58 +0530

    This is server file which contains backend

---
+++ server/router/auth-router.js
@@ -0,0 +1,38 @@
+// *------------------------
+//* Routers
+// *------------------------
+
+
+const express = require('express');
+const router = express.Router();
+// const home = require("../controllers/auth-controller");
+// const register = require("../controllers/auth-controller");
+// const login = require("../controllers/auth-controller");
+const authControllers = require('../controllers/auth-controller');
+
+router.route("/").get(authControllers.home);
+
+router.route("/register").post(authControllers.register);
+// router.route("/register").post(authControllers.register);
+
+router.route("/login").get(authControllers.login);
+
+
+
+// router.route("/").get((req,res) => {
+    // res
+    //     .status(200)
+    //     .send("Welcome to Home Page with Router.");
+// });
+
+// router.get("/", (req,res) => {
+//     res.status(200).send("Welcome to Home Page - Router.");
+// });
+
+// router.route("/register").get((req,res) => {;
+//     res
+//         .status(200)
+//         .send("Hello welcome to Register Page Router.");
+// });
+
+module.exports = router;
